1d85c5929dd4344abe2c235b348cd1be2e139fb1
Howard Pinsley <hpinsley@gmail.com>
2016-05-09 21:31:52 -0400

    Incorporate bootstrap and jquery and create a css gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var open = require('gulp-open');            // Open a url in a web browser
 var browserify = require('browserify');     // Bundles JS
 var reactify = require('reactify');         // Transforms React JSX to js
 var source = require('vinyl-source-stream');    // Use conventional text streams with gulp
+var concat = require('gulp-concat');
 
 var config = {
     port: 9005,
@@ -13,6 +14,10 @@ var config = {
     paths: {
         html: './src/*.html',
         js: './src/**/*.js',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+        ],
         dist: './dist',
         mainJs: './src/main.js'
     }
@@ -40,6 +45,12 @@ gulp.task('html', function(){
         .pipe(connect.reload());
 });
 
+gulp.task('css', function() {
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
 gulp.task('js', function(){
     browserify(config.paths.mainJs)
     .transform(reactify)
@@ -55,4 +66,4 @@ gulp.task('watch', function() {
     gulp.watch(config.paths.js, ['js']);
 });
 
-gulp.task('default', ['html', 'js', 'open', 'watch']);
+gulp.task('default', ['html', 'css', 'js', 'open', 'watch']);
